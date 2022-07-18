63bace6e243611a9a0b20316b1a9da67ddbbcfb4
jmtgalvez <jmtgalvez@gmail.com>
2022-07-18 14:33:01 +0800

    Renamed landing page and home page

--- capstone-client/src/App.js
+++ capstone-client/src/App.js
@@ -1,27 +1,27 @@
 import React, { useState, useEffect } from "react";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import FrontPage from "./components/Home/FrontPage";
+import LandingPage from "./components/Home/LandingPage";
 import Register from "./components/Forms/Register/Register";
 import Login from "./components/Forms/Login/Login";
 
 import { UserContext } from "./components/UserContext";
-import UiFrontPage from "./components/UI/UiFrontPage";
+import HomePage from "./components/UI/HomePage";
 
 function App() {
     const [user, setUser] = useState(null);
 
     useEffect( () => {
-        // console.log(user);
+        console.log(user);
     }, [user])
 
     return(
         <UserContext.Provider value={{user, setUser}}>
             <Router>
                 <Routes>
-                    <Route path='/' element={<FrontPage />}/>
+                    <Route path='/' element={<LandingPage />}/>
                     <Route path='/Register' element={<Register />}/>
                     <Route path='/Login' element={<Login />}/>
-                    <Route path='/Home' element={<UiFrontPage />}/>
+                    <Route path='/Home' element={<HomePage />}/>
                 </Routes>
             </Router>
         </UserContext.Provider>
