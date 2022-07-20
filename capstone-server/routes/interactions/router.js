7e51b12409b7c608ecf0361abf1f4eb09fc0d79f
jmtgalvez <jmtgalvez@gmail.com>
2022-07-20 11:39:37 +0800

    Added user interactions

---
+++ capstone-server/routes/interactions/router.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+
+const CTRL = require('./controller');
+
+router.post('/follow/:following_id', async (req, res) => {
+  try {
+    await CTRL.checkUserExists(req.params.following_id);
+
+    CTRL.follow({ req.body.user_id, req.params.following_id })
+      .then( result => {
+        res.status(200)
+           .json({
+            status: 200,
+            message: 'Follow operation success',
+           })
+      })
+  } catch (status) {
+    res.status(status).json({ status });
+  }
+})
+
+router.post('/like/:post_id', async (req, res) => {
+  try {
+    await CTRL.checkPostExists(req.params.post_id);
+
+    CTRL.like({ req.body.user_id, req.params.post_id })
+      .then( result => {
+        res.status(200)
+           .json({
+            status: 200,
+            message: 'Like operation success',
+           })
+      })
+
+  } catch (status) {
+    res.status(status).json({ status });
+  }
+})
+
+router.post('/chat', async (req, res) => {
+  try {
+
+  } catch (status) {
+    res.status(status).json({ status });
+  }
+})
+
+module.exports = router;
