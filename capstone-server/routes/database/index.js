cb0e8767f4b7d732082e42fb5829b87530273d70
jmtgalvez <jmtgalvez@gmail.com>
2022-07-27 15:13:02 +0800

    Fixed login controller checkUser return 404 status

--- capstone-server/routes/database/index.js
+++ capstone-server/routes/database/index.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'capstone',
-    password: 'capstone',
-    database: 'capstone'
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'bd11f83fb51443',
+    password: '87eebf2e',
+    database: 'heroku_27d48bef9ad971a'
 });
 
 module.exports = db;
