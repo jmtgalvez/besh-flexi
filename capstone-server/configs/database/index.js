92830590ad3161774415681be54bd892f387774f
jmtgalvez <jmtgalvez@gmail.com>
2022-07-28 13:31:55 +0800

    Changed db connection

--- capstone-server/configs/database/index.js
+++ capstone-server/configs/database/index.js
@@ -7,32 +7,32 @@ let db = mysql.createPool({
     database: 'heroku_27d48bef9ad971a'
 });
 
-const handleDisconnect = () => {
-    db = mysql.createPool({
-        host: 'us-cdbr-east-06.cleardb.net',
-        user: 'bd11f83fb51443',
-        password: '87eebf2e',
-        database: 'heroku_27d48bef9ad971a'
-    });
+// const handleDisconnect = () => {
+//     db = mysql.createPool({
+//         host: 'us-cdbr-east-06.cleardb.net',
+//         user: 'bd11f83fb51443',
+//         password: '87eebf2e',
+//         database: 'heroku_27d48bef9ad971a'
+//     });
 
-    // db.connect((err) => {
-    //     if (err) {
-    //         console.log('Error connectiong to database ...' + err);
-    //         return setTimeout(handleDisconnect, 2000);
-    //     }
-    //     console.log('Successfully connected to database ...');
-    // });
+//     // db.connect((err) => {
+//     //     if (err) {
+//     //         console.log('Error connectiong to database ...' + err);
+//     //         return setTimeout(handleDisconnect, 2000);
+//     //     }
+//     //     console.log('Successfully connected to database ...');
+//     // });
 
-    db.on('error', (err) => {
-        console.log(`DB Error: ${err}`);
-        if ( err.code === 'PROTOCOL_CONNECTION_LOST' ) {
-            handleDisconnect();
-        } else {
-            throw err;
-        }
-    })
-}
+//     db.on('error', (err) => {
+//         console.log(`DB Error: ${err}`);
+//         if ( err.code === 'PROTOCOL_CONNECTION_LOST' ) {
+//             handleDisconnect();
+//         } else {
+//             throw err;
+//         }
+//     })
+// }
 
-handleDisconnect();
+// handleDisconnect();
 
 module.exports = db;
